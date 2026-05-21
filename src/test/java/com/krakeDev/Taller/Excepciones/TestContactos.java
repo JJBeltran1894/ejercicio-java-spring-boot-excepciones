0417992e1b84fa0a8bdb2ebbd1b4b0b920e50fce
JJBeltran1894 <juanjbeltran94@hotmail.com>
2026-05-21 05:56:01 -0500

    JJBA: Agregada la clase TestContactos: Se valida el funcionameinto de las clases GuardarContacto y LeerContacto

---
+++ src/test/java/com/krakeDev/Taller/Excepciones/TestContactos.java
@@ -0,0 +1,13 @@
+package com.krakeDev.Taller.Excepciones;
+
+public class TestContactos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		GuardarContacto.guardar();
+	
+		LeerContacto.leer();
+	}
+	
+
+}
